78a8b4da99745731d240e3427cb38d50967eefab
alinaqi12 <salinaqi57@gmail.com>
2023-08-30 09:20:13 +0500

    Commit

--- BACKEND.py
+++ BACKEND.py
@@ -63,45 +63,16 @@ def getdatabases():
 @app.route('/getdata', methods=['POST'])
 def getgraph():
     if request.method == 'POST':
-        nodes_and_edges, image_data = getdata(request.get_json())
-        # print(nodes_and_edges)
-        # Check if there was an error
-        if isinstance(nodes_and_edges, str):
-            return jsonify({'error': nodes_and_edges})
+        nodes_and_edges= getdata(request.get_json())
+        # print(type(nodes_and_edges))
+        return jsonify(nodes_and_edges)   
 
-        # Create a response with JSON data and set the content-type
-        response_data = {
-            'graph_data': nodes_and_edges,
-        }
-
-        # You can also set other headers as needed, such as Access-Control-Allow-Origin
-        headers = {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-        }
-
-        # Create a Flask response with the image data and set the content-type
-        image_response = Response(image_data, content_type='image/jpeg')
-
-        # Combine both responses into one
-        response = app.response_class(
-            response=json.dumps(response_data),  # Use json.dumps from the json module
-            status=200,
-            headers=headers,
-            mimetype='application/json'
-        )
-
-        response.set_data(image_response.get_data())
-        response.status_code = 200
-
-        return response   
-    
 @app.route('/upload_csv_neo', methods=['POST'])
 def csv():
     response=upload_csv(request)
     return response 
 
-@app.route('/upload_Jformat_neo', methods=['POST'])
+@app.route('/upload_json_neo', methods=['POST'])
 def json():
     response=import_jformat_data(request)
     return response
