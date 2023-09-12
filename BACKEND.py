e4e162d4a61cd0df90c11bdb503e3845ca675847
FatimaTuZahra1 <fatimatuzahra731@gmail.com>
2023-09-12 14:36:54 +0500

    Merge branch 'main' of https://github.com/alinaqi12/BACKEND into fatima

--- BACKEND.py
+++ BACKEND.py
@@ -123,7 +123,8 @@ def filternodes():
         response = get_nodes_and_edges()  # Call the function
         return response
 
+#if __name__ == '__main__':
+#   app.run(host="192.168.18.95",debug=True, port=34464)
+
 if __name__ == '__main__':
-    app.run(host="192.168.18.95",debug=True, port=34464)
-
-
+    app.run(host="192.168.137.229",debug=True, port=34464)
